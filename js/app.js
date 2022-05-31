0c96105940284ae75c3f8220e2db32db15d449a8
Moklesur Rahman <80785811+Moklesur-Rahman2@users.noreply.github.com>
2022-05-31 10:36:13 +0600

    upload

---
+++ js/app.js
@@ -0,0 +1,102 @@
+// search button click event
+document.getElementById('search-button').addEventListener('click', () => {
+    // get input field keyword
+    const keyword = document.getElementById('search-input').value
+    getPhoneData(keyword)   
+})
+
+
+// Get Products data from api
+const getPhoneData = (searchText) =>{
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    .then(res => res.json())
+    .then(phoneList => showPhone(phoneList.data))
+}
+
+
+// Show Products to ui
+const showPhone = (phones) => {
+    // Get product div from ui
+    const keyword = document.getElementById('search-input').value
+    const phoneList = document.getElementById('phone-list')
+    const phoneDetailsDiv = document.getElementById('phone-details')
+    const alert = document.getElementById('alert')
+    phoneList.innerText = ''
+    phoneDetailsDiv.innerHTML = ''
+
+    if(phones.length === 0){
+        alert.innerText = `Sorry! "${keyword}" Not Found`
+    }else{
+        alert.innerText = ''
+        const maxProduct = phones.slice(0, 20)
+        maxProduct.forEach(phone => {   
+            // create a div for each product
+            const phoneDiv = document.createElement('div')
+            phoneDiv.classList.add('card')
+    
+            phoneDiv.innerHTML = `
+                <img src="${phone.image}" class="card-img-top" alt="${phone.phone_name}">
+                <div class="card-body text-center">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+                <a href="#" class="btn btn-primary" id="details" onclick="getDetails('${phone.slug}')">More Details</a>
+                </div>
+                `
+                phoneList.append(phoneDiv)
+        });
+    }
+
+    // reset input field
+    document.getElementById('search-input').value = ''
+}
+
+
+// Get Product details click event
+const getDetails = (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(phoneDetails => showDetails(phoneDetails.data))
+}
+
+// show product details function
+const showDetails = (phone) => {
+    const phoneDetailsDiv = document.getElementById('phone-details')
+    phoneDetailsDiv.innerHTML = `
+        <div class="card mx-auto mt-3 px-3">
+            <img src="${phone.image}" class="card-img-top mx-auto" alt="${phone.name}">
+            <div class="card-body">
+            <h5 class="card-title fs-4">${phone.name}</h5>
+            <p class="card-text">${phone.releaseDate.length != 0 ? phone.releaseDate : 'No release Date found'}</p>
+            <div class="main-features">
+                <p class="fw-bold">Main Features:</p>
+                <p><b>ChipSet:</b> ${phone.mainFeatures.chipSet}</p>
+                <p><b>Display Size:</b> ${phone.mainFeatures.displaySize}</p>
+                <p><b>Memory: </b>${phone.mainFeatures.memory}</p>
+                <p><b>Sensor: </b>${getSensor(phone.mainFeatures.sensors)}</p>
+                <p><b>Storage:</b> ${phone.mainFeatures.storage}</p>
+                <p>${phone.others != null ? `<b>Others: </b>` + getOthers(phone.others) : ''}</p>
+            </div>
+            </div>
+        </div>
+    `
+}
+
+// Get sensors function
+const getSensor = (sensors) =>{
+    let sensor = ''
+    for(const feature of sensors){
+        sensor += feature + ', '
+    }
+    return sensor
+}
+
+// Get others function
+const getOthers = (others) => {
+    const keys = Object.keys(others)
+        let othersItem = ''
+        keys.forEach((key, index) =>{
+        othersItem += `${key}: ${others[key]}` + `<br>`
+        })
+        return othersItem
+}
